0d7ca1f754ed71528a9bdf1dd1674a0e002cbf22
Keeeweee <epsilonmajorquezero@gmail.com>
2018-07-19 10:53:00 +0200

    Add Job.title

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.TextField()
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
