b65ca90c8be81ec98fc89940388b63e22480001b
Keeeweee <epsilonmajorquezero@gmail.com>
2018-08-27 13:41:17 +0200

    Add defaults for some text fields in job model

--- jobs/models.py
+++ jobs/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.TextField()
-    title = models.TextField()
-    company = models.CharField(max_length=255)
+    summary = models.TextField(default='')
+    title = models.TextField(default='')
+    company = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return self.company
