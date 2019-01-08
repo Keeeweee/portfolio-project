9917c42f9cd89cc595c33e7ca80a3e8bad914701
Keewee <epsilonmajorquezero@gmail.com>
2019-01-08 15:10:57 +0100

    Add urls for routing to new Exp Sums app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 import jobs.views
 import blog.urls
 import frasometre.urls
+import expsum.urls
 
 urlpatterns = [
                 path('admin/', admin.site.urls),
                 path('', jobs.views.home, name="home"),
                 path('blog/', include(blog.urls)),
                 path('frasometre/', include(frasometre.urls)),
+                path('expsum/', include(expsum.urls)),
             ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
