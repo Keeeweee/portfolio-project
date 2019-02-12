d6e51db35dc474147eb52fc34914aa694bb6e826
Keewee <epsilonmajorquezero@gmail.com>
2019-02-12 18:06:16 +0100

    Format Tweets before passing them to the view

--- twitter_broadcast/views.py
+++ twitter_broadcast/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from twitter_broadcast import twitter_reader as twt
 from portfolio import local_settings as ls
+from twitter_broadcast.tweet import *
 
 
 # Create your views here.
 def twitter_broadcast(request):
-    # consumerKey = request.session.get('consumerKey')
-    # print('Consumer Key is: ' + consumerKey)
-    # consumerSecret = request.session.get('consumerSecret')
-    # accessTokenKey = request.session.get('accessTokenKey')
-    # accessTokenSecret = request.session.get('accessTokenSecret')
     twitter = twt.Twitter(ls.consumerKey, ls.consumerSecret, ls.accessTokenKey, ls.accessTokenSecret)
 
-    # q = twitter.query('pizza')
-    # for item in q.get_iterator():
-    #     print(item['user']['screen_name'], item['text'])
-
     q = twitter.getUserTimeline('epsilonkeewee')
+    tweets = []
     for item in q.get_iterator():
-        print(item['user']['screen_name'], item['text'])
+        print(item['user']['screen_name'], item)
+        tweets.append(Tweet(item))
 
-    return render(request, 'twitter_broadcast/index.html')
+    return render(request, 'twitter_broadcast/index.html', {'tweets': tweets})
