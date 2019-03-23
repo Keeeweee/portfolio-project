487a3bd383d3706a9a7f9667c1a2c75835499a11
Keewee <epsilonmajorquezero@gmail.com>
2019-03-23 11:04:45 +0100

    Rename matrix app html

--- matrix/views.py
+++ matrix/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def matrix(request):
-    return render(request, 'matrix/index.html')
+    return render(request, 'matrix/matrix.html')
