e0c3c422ae17c997701d0593220a1bd7dbda92de
Keeeweee <epsilonmajorquezero@gmail.com>
2019-04-23 17:56:31 +0200

    Add start and end date to job model

--- jobs/models.py
+++ jobs/models.py
@@ -7,6 +7,8 @@ class Job(models.Model):
     summary = models.TextField(default='')
     title = models.TextField(default='')
     company = models.CharField(max_length=255, default='')
+    start = models.DateField(default=None, blank=True, null=True)
+    end = models.DateField(default=None, blank=True, null=True)
 
     def __str__(self):
         return self.company
