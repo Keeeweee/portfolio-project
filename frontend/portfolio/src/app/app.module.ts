35e1ab8fd8f4f2f9f578bb058814d2b67f191641
Keeeweee <epsilonmajorquezero@gmail.com>
2019-08-20 18:04:54 +0200

    Add new components to the app module

--- frontend/portfolio/src/app/app.module.ts
+++ frontend/portfolio/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {JobsComponent} from "./jobs/jobs.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent,
+    NavbarComponent
   ],
   imports: [
+    // HttpModule,
     BrowserModule,
     AppRoutingModule
   ],
