b020bf9ae5ad701d1655835a9f821fb8a5372a46
Keeeweee <epsilonmajorquezero@gmail.com>
2019-08-23 16:41:49 +0200

    Add footer component

--- frontend/portfolio/src/app/app-routing.module.ts
+++ frontend/portfolio/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {JobsComponent} from "./jobs/jobs.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [    {
+        path:"jobs",
+        component: JobsComponent,
+    },
+    // {
+    //     path:"**",
+    //     component: PagenotfoundComponent,
+    // }
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
