5a07becde0f591c0929c855656847241f47048b8
Keewee <epsilonmajorquezero@gmail.com>
2019-08-26 16:38:41 +0200

    Add angular app settings

---
+++ frontend/portfolio/src/app/app.settings.ts
@@ -0,0 +1,5 @@
+export class AppSettings {
+  public static backendUrl = 'http://localhost:8000';
+  public static mediaUrl = AppSettings.backendUrl + '/media';
+  public static jobsApiUrl = AppSettings.backendUrl + '/jobs/api';
+}
