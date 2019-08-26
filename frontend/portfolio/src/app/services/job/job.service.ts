909a9ad6b437d97d223c702e9244fce3255fd56e
Keewee <epsilonmajorquezero@gmail.com>
2019-08-26 16:41:24 +0200

    Use backend to get jobs information in jobs.service

--- frontend/portfolio/src/app/services/job/job.service.ts
+++ frontend/portfolio/src/app/services/job/job.service.ts
@@ -1,19 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Job} from "../../jobs/job/job.class";
 import {Observable, of} from "rxjs";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
+import {AppSettings} from "../../app.settings";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  job: Job = new Job(1, "url", "Company", new Date(2019,1, 1), new Date(2019, 2, 1), "Title", "Summary");
+  private jobsUrl = AppSettings.jobsApiUrl + '/jobs';
 
-  jobs = [this.job, this.job, this.job];
-
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   getJobs(): Observable<Job[]> {
-    return of(this.jobs);
+    return this.http.get<any>(this.jobsUrl).pipe(map(jobs => JSON.parse(jobs).map(job => Job.fromJson(job))))
+                                           .pipe(catchError(this.handleError<Job[]>('getJobs', [])));
+  }
+
+  getJob(id: number): Observable<Job> {
+    const url = `${this.jobsUrl}/${id}`;
+    return this.http.get<Job>(url).pipe(catchError(this.handleError<Job>(`getjob id=${id}`)))
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
