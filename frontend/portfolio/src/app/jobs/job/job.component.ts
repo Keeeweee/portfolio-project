fc5d863908578389d5ef25b1feba70fd4df8c447
Keewee <epsilonmajorquezero@gmail.com>
2019-08-26 16:44:47 +0200

    Use app constants in job.component

--- frontend/portfolio/src/app/jobs/job/job.component.ts
+++ frontend/portfolio/src/app/jobs/job/job.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Job} from "./job.class";
+import {AppSettings} from "../../app.settings";
 
 @Component({
   selector: 'job-view',
@@ -7,6 +8,7 @@ import {Job} from "./job.class";
   styleUrls: ['./job.component.scss']
 })
 export class JobComponent implements OnInit {
+  private appSettings = AppSettings;
 
   @Input() job: Job;
   constructor() { }
