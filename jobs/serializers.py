fa5181942c656f93ddd2ec71fd3dd880a03dc340
Keewee <epsilonmajorquezero@gmail.com>
2019-08-28 14:48:20 +0200

    Add UserSerializer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from jobs.models import Job
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-
         fields = ['id', 'image', 'summary', 'title', 'company', 'start', 'end']
